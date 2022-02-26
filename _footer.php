61e9eb6d024049d96b0be600d38be8f79397663d
DanielSaInfo <97560656+DanielSaInfo@users.noreply.github.com>
2022-02-25 21:22:51 -0300

    php

---
+++ _footer.php
@@ -0,0 +1,16 @@
+<?php // ################# Quebra do template ################# 
+?>
+
+</main>
+
+<?php // Rodapé 
+?>
+<footer>
+
+    &copy; Copyright 2022 <?php echo $site_name ?>.
+
+</footer>
+
+</body>
+
+</html>

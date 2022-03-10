4d478ebc3caea366760a087e786a0f2e3bc0a2b3
DanielSaInfo <97560656+DanielSaInfo@users.noreply.github.com>
2022-03-09 21:33:42 -0300

    090322

--- PHP_DB_01/_config.php
+++ PHP_DB_01/_config.php
@@ -1,43 +1,60 @@
 <?php
-
-// Configura a página de caracteres do PHP para UTF-8
-// DEVE ser a primeira linha de código do site.
+/*
+ * Configura a página de caracteres do PHP para UTF-8
+ * DEVE ser a primeira linha de código do site.
+ */
 header("Content-type: text/html; charset=utf-8");
 
-// Conexão com o banco de dados MySQL (endereço do servidor MySQL, nome de usuário, senha do db, nome do banco de dados)
-$conn = new mysqli('localhost', 'root', '', 'php_app');
-
-// Testa se conexão foi bem sucedida.
-if ($conn->connect_error) {
-    die("Falha de conexão com banco de dados: " . $conn->connect_error);
-}
-
-// Consulta de teste. Apague depois de testar.
-$result = $conn->query("SELECT * FROM articles");
-
-while ($row = $result->fetch_assoc()) {
-    echo '<pre>';
-    echo $row['article_title'] . ' - ' . $row['article_intro'];
-    echo '</pre>';
-}
-
-// Teste de INSERT. Apague depois de testar.
-$conn->query("INSERT INTO contacts (contact_name, contact_email, contact_subject, contact_message ) VALUES ('Joca', 'joca@email', 'Teste', 'Mensagem do joca')");
-
-echo '<hr><hr>';
-
-/**
- * Variáveis do tema
+/*
+ * Faz conexão com MySQL/MariaDB.
+ * Os dados da conexão estão em "/_config.ini".
  */
+
+// Armazena o arquivo "/_config.ini" em um array "$i"...
+$i = parse_ini_file($_SERVER['DOCUMENT_ROOT'] . '/_config.ini', true);
+
+// Itera cada chve do array...
+foreach ($i as $key => $value) :
+
+    // Se a chave tem o mesmo nome do servidor...
+    if ($_SERVER['SERVER_NAME'] === $key) :
+
+        // Conexão com MySQL/MariaDB usando "mysqli" (orientada a objetos)
+        @$conn = new mysqli($value['server'], $value['user'], $value['password'], $value['database']);
+
+        // Trata possíveis exceções
+        if ($conn->connect_error) die("Falha de conexão com o banco e dados: " . $conn->connect_error);
+    endif;
+endforeach;
+
+// Seta transações com MySQL/MariaDB para UTF-8.
+$conn->query("SET NAMES 'utf8'");
+$conn->query('SET character_set_connection=utf8');
+$conn->query('SET character_set_client=utf8');
+$conn->query('SET character_set_results=utf8');
+
+// Seta dias da semana e meses do MySQL/MariaDB para "português do Brasil".
+$conn->query('SET GLOBAL lc_time_names = pt_BR');
+$conn->query('SET lc_time_names = pt_BR');
+
+// Define o fuso horário (opcional).
+date_default_timezone_set('America/Sao_Paulo');
+
+/**********************
+ * Variáveis do tema. *
+ **********************/
+/*
+ * Dica: No futuro, essas variáveis poderiam ser obtidas do banco de dados.
+ */ 
 
 // Nome do aplicativo
 $site_name = "Decodificando";
 
-// Slogan do site
+// Slogan do aplicativo
 $site_slogan = "Just Code it.";
 
-// Logotipo do site
-$site_logo = "/img/logo_decodificando.jpg";
+// Logotipo do aplicativo
+$site_logo = "/img/logo_decodificando.png";
 
 // Proprietário do aplicativo (mensagem de copyright)
 $site_owner = "Turma 2021.1";
@@ -48,5 +65,38 @@ $page_title = "";
 // Define o link ativo no menu principal
 $page_menu = "";
 
-// Define o fuso horário (opcional).
-date_default_timezone_set('America/Sao_Paulo');
+/********************
+ * Funções globais. *
+ ********************/
+
+// Função que sanitiza campos de formulário.
+function sanitize($field_name, $field_type)
+{
+
+    // Variável com valor do campo filtrado.
+    $field_value = '';
+
+    // Aplica o filtro adequado, de acordo com o tipo de campo.
+    switch ($field_type):
+
+            // Se é um campo 'string', remove caracteres perigosos.
+        case 'string':
+            $field_value = htmlspecialchars($_POST[$field_name]);
+            break;
+
+            // Se é um campo 'email', remove caracteres inválidos.
+        case 'email':
+            $field_value = filter_input(INPUT_POST, $field_name, FILTER_SANITIZE_EMAIL);
+            break;
+
+    endswitch;
+
+    // Remove espaços duplicados no meio da string.
+    $field_value = preg_replace('/\\s\\s+/', ' ', $field_value);
+
+    // Remove espaços antes e depois da string.
+    $field_value = trim($field_value);
+
+    // Retorna o valor do campo já sanitizado.
+    return $field_value;
+}
--- PHP_DB_01/page/contacts.php
+++ PHP_DB_01/page/contacts.php
@@ -3,6 +3,110 @@
 // Inclui arquivo de configuração
 require_once $_SERVER['DOCUMENT_ROOT'] . "/_config.php";
 
+/*******************************************
+ * Seu código PHP desta página entra aqui! *
+ *******************************************/
+
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+// Variáveis desta página
+$name = $email = $subject = $message = $feedback = '';
+
+// Variável que exibe/oculta formulário.
+$show_form = true;
+
+// Detecta se o formulário foi enviado...
+if (isset($_POST['send'])) {
+
+    // Se foi enviado, processa o formulário...
+
+    // Obtém os valores dos campos, sanitiza e armazena nas variáveis.
+    // Atenção! A função "sanitize()" está em "/_config.php".
+    $name = sanitize('name', 'string');
+    $email = sanitize('email', 'email');
+    $subject = sanitize('subject', 'string');
+    $message = sanitize('message', 'string');
+
+    // Verifica se todos os campos form preenchidos
+    if ($name === '' or $email === '' or $subject === '' or $message === '') :
+        $feedback = '<h3 style="color:red">Erro: por favor, preencha todos os campos!</h3>';
+    else :
+
+        /*
+    // Isso é somente para testes. Remova depois dos testes.
+    echo '<pre>';
+    print_r($_POST);
+    echo '</pre><hr><hr>';
+    */
+
+        // Cria a query para slvar no banco de dados.
+        $sql = <<<SQL
+
+INSERT INTO contacts (
+    contact_name, 
+    contact_email, 
+    contact_subject, 
+    contact_message
+) VALUES (
+    '{$name}', 
+    '{$email}',
+    '{$subject}',
+    '{$message}'
+);
+
+SQL;
+
+        // Salva contato no banco de dados.
+        $conn->query($sql);
+
+        // Obtém somente primeiro nome do rementente.
+        $first_name = explode(" ", $name)[0];
+
+        // Cria mensagem de confirmação.
+        $feedback = <<<OUT
+        
+<h3>Olá {$first_name}!</h3>
+<p>Seu contato foi enviado com sucesso.</p>
+<p><em>Obrigado...</em></p>
+
+OUT;
+
+        // Oculto o formulário.
+        $show_form = false;
+
+        // Data de envio.
+        $now = date('d/m/Y \à\s H:i');
+
+        // Enviar e-mail para o administrador.
+
+        $to = 'adm@ptp_db_01.com';
+        $sj = 'Alguém entrou em contato';
+        $msg = <<<MSG
+
+Contato enviado pelo formulário do site:
+
+    Data: {$now}
+    Remetente: {$name}
+    E-mail: {$email}
+    Assunto: {$subject}
+    -----------------------------
+    {$message}
+            
+MSG;
+
+        @mail($to, $sj, $msg);
+
+    endif;
+}
+
+/*********************************************
+ * Seu código PHP desta página termina aqui! *
+ *********************************************/
+
 // Define o título DESTA página.
 $page_title = "Faça contato";
 
@@ -14,39 +118,53 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/_header.php";
 
 ?>
 
-<?php // Conteúdo ?>
+<?php // Conteúdo 
+?>
 <article>
 
-    <h2>Faça Contato</h2>
+    <h2>Faça contato</h2>
+    <p>Preencha todos os campos do formulário abaixo para entrar em contato com a equipe do <strong><?php echo $site_name ?></strong>.</p>
 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil obcaecati id recusandae minus porro
-        laudantium rem. Similique repellendus incidunt ad labore unde voluptates, recusandae at, expedita magnam
-        iure facere quia?</p>
+    <?php echo $feedback; ?>
 
-    <h3>Links:</h3>
+    <?php if ($show_form) : ?>
 
-    <ul>
-        <?php // O atributo 'target="_blank"' do link força a abertura em outra guia do navegador ?>
-        <li><a href="http://catabits.com.br" target="_blank">Site do Fessô</a></li>
-        <li><a href="https://americanas.com" target="_blank">Site Hackeado</a></li>
-        <li><a href="https://www.rj.senac.br" target="_blank">Senac RJ</a></li>
-    </ul>
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
+            <input type="hidden" name="send" value="true">
 
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque quod suscipit ratione commodi,
-        corrupti tempore mollitia accusantium in eligendi dolores dicta dolore, accusamus tenetur omnis, dolor
-        ducimus! Iure, ad ea!</p>
+            <p>
+                <label for="name">Seu nome:</label>
+                <input type="text" name="name" id="name" placeholder="Preencha seu nome completo." value="<?php echo $name ?>" autofocus>
+            </p>
 
-    <div>
-        <img src="https://picsum.photos/400/200" alt="Imagem aleatória">
-    </div>
+            <p>
+                <label for="email">Seu e-mail:</label>
+                <input type="email" name="email" id="email" placeholder="Seu e-mail principal." value="<?php echo $email ?>">
+            </p>
 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam maxime a saepe voluptatum laborum
-        magnam, temporibus blanditiis aspernatur, nihil vero consequuntur quidem perferendis aliquam. Rem
-        voluptatibus consequuntur neque ex explicabo!</p>
+            <p>
+                <label for="subject">Assunto:</label>
+                <input type="text" name="subject" id="subject" placeholder="Um resumo do seu contato." value="<?php echo $subject ?>">
+            </p>
+
+            <p>
+                <label for="message">Mensagem:</label>
+                <textarea name="message" id="message" placeholder="Escreva o quanto precisar."><?php echo $message ?></textarea>
+            </p>
+
+            <p>
+                <label></label>
+                <button type="submit">Enviar</button>
+            </p>
+
+        </form>
+
+    <?php endif; ?>
 
 </article>
 
-<?php // Barra lateral ?>
+<?php // Barra lateral 
+?>
 <aside>
 
     <h3>Seções:</h3>
